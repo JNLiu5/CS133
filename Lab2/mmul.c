f07608b32dd2bf072afc592e8490bfc7344ebab8
Justin <JNLiu5@gmail.com>
2017-05-10 03:29:51 -0700

    Loop tiling

--- Lab2/mmul.c
+++ Lab2/mmul.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define block_size 64
+#define block_size 16
 
 void mmul(float *A, float *B, float *C, int n)  {
     int rank, size, rows, i, j, k;
@@ -24,6 +24,26 @@ void mmul(float *A, float *B, float *C, int n)  {
     
     float C_local[rows * n];
 
+    for(i = 0; i < rows * n; i++) {
+        C_local[i] = 0;
+    }
+    
+    int bi, bj;
+    int i_blocks = rows / block_size;
+    int j_blocks = n / block_size;
+
+    for(i = 0; i < rows; i += block_size) {
+        for(j = 0; j < n; j += block_size) {
+            for(k = 0; k < n; k++) {
+                for(bi = i; bi < i + block_size; bi++) {
+                    for(bj = j; bj < j + block_size; bj++) {
+                        C_local[bi * n + bj] += A_local[bi * n + k] * B[k * n + bj];
+                    }
+                }
+            }
+        }
+    }
+    /*
     for(i = 0; i < rows; i++) {
        for(j = 0; j < n; j++) {
            C_local[i * n + j] = 0;
@@ -32,6 +52,7 @@ void mmul(float *A, float *B, float *C, int n)  {
            }
        }
     }
+    */
 
     MPI_Gather(C_local, rows * n, MPI_FLOAT, C, rows * n, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
